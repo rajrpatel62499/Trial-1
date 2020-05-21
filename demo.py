7c4ad50aab30ce7de2f1c3c2b11fc6a815787b13
rajrpatel62499 <31943198+rajrpatel62499@users.noreply.github.com>
2020-05-21 18:22:49 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,1 +1,16 @@
-print("hello world")
+# Import socket module 
+import socket			 
+
+# Create a socket object 
+s = socket.socket()		 
+
+# Define the port on which you want to connect 
+port = 12000				
+
+# connect to the server on local computer 
+s.connect(('103.228.146.208', port)) 
+
+# receive data from the server 
+print(s.recv(1024))
+# close the connection 
+s.close()	 
